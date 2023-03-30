3450eeafc7c6d2a01d24e7ce7c185c4d69a1b8ac
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-03-30 18:50:22 -0300

    create route to show a list of programming languages

--- src/main/java/br/com/hvtt/languages/api/LanguageController.java
+++ src/main/java/br/com/hvtt/languages/api/LanguageController.java
@@ -1,12 +1,22 @@
 package br.com.hvtt.languages.api;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguageController {
-  @GetMapping(value = "/preferred-language")
-  public String processPreferredLanguage() {
-    return "Olá, Java!";
+  private List<Language> languages = List.of(
+      new Language("Java",
+          "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png",
+          1),
+      new Language("PHP",
+          "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png",
+          2));
+
+  @GetMapping("/languages")
+  public List<Language> getLanguages() {
+    return languages;
   }
 }
