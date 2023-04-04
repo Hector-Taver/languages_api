e3a2eb025ea1329f13c7c339a619bb9d71281ff6
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-04-04 20:52:48 -0300

    crud: get language by id

--- src/main/java/br/com/hvtt/languages/api/LanguageController.java
+++ src/main/java/br/com/hvtt/languages/api/LanguageController.java
@@ -3,10 +3,13 @@ package br.com.hvtt.languages.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class LanguageController {
@@ -18,6 +21,11 @@ public class LanguageController {
     List<Language> languages = repository.findAll();
 
     return languages;
+  }
+
+  @GetMapping("/languages/{id}")
+  public Language getLanguageById(@PathVariable String id) {
+    return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
   }
 
   @PostMapping("/languages")
