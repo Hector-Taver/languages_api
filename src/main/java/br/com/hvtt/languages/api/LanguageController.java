734a0f8f9c7ee0fe1f6ced96c0371728f8d85c35
Hector van Tol Taver <hectorvttaver@gmail.com>
2023-04-04 21:40:33 -0300

    crud: delete language

--- src/main/java/br/com/hvtt/languages/api/LanguageController.java
+++ src/main/java/br/com/hvtt/languages/api/LanguageController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,10 @@ public class LanguageController {
 
     language.setId(id);
     return repository.save(language);
+  }
+
+  @DeleteMapping("/languages/{id}")
+  public void deleteLanguage(@PathVariable String id) {
+    repository.deleteById(id);
   }
 }
